b9a22a1ed1f31993a5fb622819b47b1c4694d9bf
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2021-12-23 09:00:49 +0000

    Add migrations, custom error pages, update variable names

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,21 @@
 from datetime import datetime
+
 from flask import render_template, session, redirect, url_for
 
-from . import main
+from . import main_blueprint
 from .forms import NameForm
 from .. import db
 from ..models import User
 
 
-@main.route("/", methods=["GET", "POST"])
+@main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         # ...
-        return redirect(url_for(".index"))
+        return redirect(url_for("main_blueprint.index"))
+        # can also be written as:
+        # return redirect(url_for(".index"))
     context = {
         "form": form,
         "name": session.get("name"),
